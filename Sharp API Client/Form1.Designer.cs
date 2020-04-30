100f1a8022235f7da3a275671797701ac92b4e15
sigmade <sychyov1991@mail.ru>
2020-04-30 23:08:54 +0300

    Start

--- Sharp API Client/Form1.Designer.cs
+++ Sharp API Client/Form1.Designer.cs
@@ -28,21 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.GoBtn = new System.Windows.Forms.Button();
+            this.Idlabel = new System.Windows.Forms.Label();
+            this.Titlelabel = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // GoBtn
+            // 
+            this.GoBtn.Location = new System.Drawing.Point(397, 323);
+            this.GoBtn.Name = "GoBtn";
+            this.GoBtn.Size = new System.Drawing.Size(146, 52);
+            this.GoBtn.TabIndex = 0;
+            this.GoBtn.Text = "Получить данные";
+            this.GoBtn.UseVisualStyleBackColor = true;
+            this.GoBtn.Click += new System.EventHandler(this.GoBtn_Click);
+            // 
+            // Idlabel
+            // 
+            this.Idlabel.AutoSize = true;
+            this.Idlabel.Location = new System.Drawing.Point(52, 45);
+            this.Idlabel.Name = "Idlabel";
+            this.Idlabel.Size = new System.Drawing.Size(18, 13);
+            this.Idlabel.TabIndex = 1;
+            this.Idlabel.Text = "ID";
+            // 
+            // Titlelabel
+            // 
+            this.Titlelabel.AutoSize = true;
+            this.Titlelabel.Location = new System.Drawing.Point(55, 75);
+            this.Titlelabel.Name = "Titlelabel";
+            this.Titlelabel.Size = new System.Drawing.Size(27, 13);
+            this.Titlelabel.TabIndex = 2;
+            this.Titlelabel.Text = "Title";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(58, 118);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(485, 179);
+            this.textBox.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(608, 413);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.Titlelabel);
+            this.Controls.Add(this.Idlabel);
+            this.Controls.Add(this.GoBtn);
             this.Name = "Form1";
             this.Text = "Sharp Api";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button GoBtn;
+        private System.Windows.Forms.Label Idlabel;
+        private System.Windows.Forms.Label Titlelabel;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
--- Sharp API Client/Form1.cs
+++ Sharp API Client/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +23,35 @@ namespace Sharp_API_Client
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void GoBtn_Click(object sender, EventArgs e)
+        {
+            string json = "{\"method_name\":\"feed_get\",\"private_key\":\"f120a99e16f077df3d73e39e78692b5bdb6c42fb4e56f1d65d125ac663747772\",\"m\":\"1\",\"id\":\"9\" }";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create("http://api.ru/api");
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "application/json";
+            using (var requestStream = httpRequest.GetRequestStream())
+            using (var writer = new StreamWriter(requestStream))
+            {
+                writer.Write(json);
+            }
+            using (var httpResponse = httpRequest.GetResponse())
+            using (var responseStream = httpResponse.GetResponseStream())
+            using (var reader = new StreamReader(responseStream))
+            {
+                string response = reader.ReadToEnd();
+                dynamic res = JsonConvert.DeserializeObject(response);
+
+                string id = res.response.id;
+                string title = res.response.title;
+                string text = res.response.text;
+                
+                Idlabel.Text = id;
+                Titlelabel.Text = title;
+                textBox.Text = text;
+            }
         }
     }
 }
